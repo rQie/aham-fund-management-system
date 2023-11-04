d6f283c0820a8603676b240a82c1c60dd11a29c9
rQie <rifqieakma@gmail.com>
2023-11-04 13:35:29 +0800

    setup vite vue

--- aham-fund-management-system/src/App.vue
+++ aham-fund-management-system/src/App.vue
@@ -12,6 +12,10 @@ import HelloWorld from './components/HelloWorld.vue'
     </a>
   </div>
   <HelloWorld msg="Vite + Vue" />
+  <v-card class="mx-auto" width="400" prepend-icon="mdi-home">
+    <template v-slot:title> This is a title </template>
+    <v-card-text> This is content </v-card-text>
+  </v-card>
 </template>
 
 <style scoped>
